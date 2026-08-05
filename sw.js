b53904ebdb1e6907cf125b5189f42d67169bbc11
frozenfireworks-lang <frozen.fireworks@gmail.com>
2026-08-05 20:14:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
-self.addEventListener("install",event=>self.skipWaiting());
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",event=>event.respondWith(fetch(event.request)));
+const CACHE='abyssal-roots-v3-6';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ e.respondWith(fetch(e.request).then(r=>{
+  const copy=r.clone();
+  caches.open(CACHE).then(c=>c.put(e.request,copy));
+  return r;
+ }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
