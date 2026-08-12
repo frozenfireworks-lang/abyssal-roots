990d2e7b3044f45bcd16ddac30a0054131dadb8d
frozenfireworks-lang <frozen.fireworks@gmail.com>
2026-08-13 02:37:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='abyssal-roots-v4-11-realtime-core';
+const CACHE='abyssal-roots-v4-13-control-stack';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
