7d4b4c71626e3874fb15e86823c61a51c519fbf8
frozenfireworks-lang <frozen.fireworks@gmail.com>
2026-08-23 00:23:36 +0900

    Increase hero sprite resolution

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='abyssal-roots-v4-27-abyssal-illumination';
+const CACHE='abyssal-roots-v4-28-hero-resolution';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./abyssal-cathedral-title.png'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
