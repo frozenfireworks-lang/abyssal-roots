8fb76ed4901575dd9d77a264fee80a05c37f8195
frozenfireworks-lang <frozen.fireworks@gmail.com>
2026-08-23 10:30:55 +0900

    Rebuild hero at native pixel resolution

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='abyssal-roots-v4-33-true-motion';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./abyssal-cathedral-title.png','./assets/hero-motion-base-v1.png'];
+const CACHE='abyssal-roots-v4-34-native-pixel';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./abyssal-cathedral-title.png'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
