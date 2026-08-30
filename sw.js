5b0746c8557a0a649704c98cd9b1ddeec5d17009
frozenfireworks-lang <frozen.fireworks@gmail.com>
2026-08-30 20:49:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='abyssal-roots-v4-36-1-single-file-art';
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./icon-192.png","./icon-512.png","./assets/hero-sheet-v435.png","./assets/enemy-sheet-v435.png","./assets/chest.png","./assets/altar.png","./assets/root_gate.png","./assets/stairs_arch.png","./assets/beacon_platform.png","./assets/pit.png","./assets/lava.png","./assets/crystal_cluster.png","./assets/wall_arch.png","./assets/lantern_pillar.png"];
+const CACHE='abyssal-roots-v4-37-optimized-128-art';
+const ASSETS=["./","./index.html"];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
